d0bb9f6fbb33f7c0e9c5dc0df1f9a7882ba04aee
Qiwen Hua <qiwen.hua@mail.utoronto.ca>
2020-08-05 13:52:19 -0700

    add ephemeral_keys api

--- index.js
+++ index.js
@@ -9,5 +9,22 @@ express()
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 const app = express();
-const stripe = require('stripe')('sk_test_51HCrBpIazuHtyn2CnkySWfrMk70lU79XbuJLFg8Z1g8KC5HlvXwki8MOv5o2k4g7C0xdQmosXIZHJk1BS5LF7LFG00nTWPzboy');
+const stripe = require('stripe')('pk_test_51HCrBpIazuHtyn2CE7C3GDruQL1WjX0dUXVV0XlT0c1rOmHHaZOa6x3JzDlfsvIRVsr1qnAsDpNZRAaBv4ZKfd0h001FQaRjX9');
 const bodyParser = require('body-parser');
+
+// noinspection JSUnresolvedFunction
+app.post('/ephemeral_keys', (req, res) => {
+    // noinspection JSUnresolvedVariable
+    const customerID = req.body.costomer_id;
+    const apiVersion = req.body.api_version;
+
+    stripe.ephemeralKeys.create({
+        customer: customerID,
+        stripe_version: apiVersion
+    }).then((key) => {
+        res.status(200).send(key)
+    }).catch((err) => {
+        console.log(err, req.body)
+        res.status(500).end()
+    });
+});
