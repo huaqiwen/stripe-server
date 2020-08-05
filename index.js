83dd2b35212e64e90c78468536406b42b0cf2c75
Qiwen Hua <qiwen.hua@mail.utoronto.ca>
2020-08-05 11:47:11 -0700

    init

--- index.js
+++ index.js
@@ -6,5 +6,8 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+const app = express();
+const stripe = require('stripe')('sk_test_51HCrBpIazuHtyn2CnkySWfrMk70lU79XbuJLFg8Z1g8KC5HlvXwki8MOv5o2k4g7C0xdQmosXIZHJk1BS5LF7LFG00nTWPzboy');
+const bodyParser = require('body-parser');
