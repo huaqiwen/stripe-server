7e943f0df22b85807264eb9206a4dc14f0c9804d
Qiwen Hua <qiwen.hua@mail.utoronto.ca>
2020-08-05 17:31:40 -0700

    test

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ express()
   .set('view engine', 'ejs')
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-console.log("hi")
-
 const app = express();
 const stripe = require('stripe')('sk_test_51HCrBpIazuHtyn2CnkySWfrMk70lU79XbuJLFg8Z1g8KC5HlvXwki8MOv5o2k4g7C0xdQmosXIZHJk1BS5LF7LFG00nTWPzboy');
 const bodyParser = require('body-parser');
+
+console.log("here")
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
