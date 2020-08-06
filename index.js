ce93b2ec802a792587ed75d7bc3e9d9b7589a899
Qiwen Hua <qiwen.hua@mail.utoronto.ca>
2020-08-05 17:40:59 -0700

    restructure

--- index.js
+++ index.js
@@ -2,39 +2,35 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-const app = express();
-const stripe = require('stripe')('sk_test_51HCrBpIazuHtyn2CnkySWfrMk70lU79XbuJLFg8Z1g8KC5HlvXwki8MOv5o2k4g7C0xdQmosXIZHJk1BS5LF7LFG00nTWPzboy');
 const bodyParser = require('body-parser');
-
-console.log("here")
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+const stripe = require('stripe')('sk_test_51HCrBpIazuHtyn2CnkySWfrMk70lU79XbuJLFg8Z1g8KC5HlvXwki8MOv5o2k4g7C0xdQmosXIZHJk1BS5LF7LFG00nTWPzboy');
 
 // noinspection JSUnresolvedFunction
-app.post('/ephemeral_keys', (req, res) => {
-    // noinspection JSUnresolvedVariable
-    const customerID = req.body.costomer_id;
-    const apiVersion = req.body.api_version;
+express()
+    .use(express.static(path.join(__dirname, 'public')))
+    .use(bodyParser.json())
+    .use(bodyParser.urlencoded({ extended: true }))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .post('/ephemeral_keys', (req, res) => {
+        // noinspection JSUnresolvedVariable
+        const customerID = req.body.costomer_id;
+        const apiVersion = req.body.api_version;
 
-    stripe.ephemeralKeys.create({
-        customer: customerID,
-        stripe_version: apiVersion
-    }).then((key) => {
-        res.status(200).send(key)
-    }).catch((err) => {
-        console.log(err, req.body)
-        res.status(500).end()
-    });
-});
+        stripe.ephemeralKeys.create({
+            customer: customerID,
+            stripe_version: apiVersion
+        }).then((key) => {
+            res.status(200).send(key)
+        }).catch((err) => {
+            console.log(err, req.body)
+            res.status(500).end()
+        });
+    })
+    .get('/', (req, res) => {
+        res.status(500).send('Hello world!')
+    })
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 // const paymentIntent = await stripe.paymentIntents.create({
 //     amount: 1500,
