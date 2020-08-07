80ed7f3fed7878d32088a6c33e0c1bef4628a152
Qiwen Hua <hqw@huaqiwen.com>
2020-08-07 09:27:52 -0700

    update

--- index.js
+++ index.js
@@ -9,9 +9,12 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+// This is currently a test secret that is for testing only
+const SECRET_KEY = 'sk_test_51HCrBpIazuHtyn2CnkySWfrMk70lU79XbuJLFg8Z1g8KC5HlvXwki8MOv5o2k4g7C0xdQmosXIZHJk1BS5LF7LFG00nTWPzboy'
+
 const bodyParser = require('body-parser');
 // noinspection JSValidateTypes
-const stripe = require('stripe')('sk_test_51HCrBpIazuHtyn2CnkySWfrMk70lU79XbuJLFg8Z1g8KC5HlvXwki8MOv5o2k4g7C0xdQmosXIZHJk1BS5LF7LFG00nTWPzboy');
+const stripe = require('stripe')(SECRET_KEY);
 
 // Prices for subscription plan charges
 const subscriptionPlan = {
