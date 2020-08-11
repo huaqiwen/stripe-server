2744b121525def1a2d143675414733970e572aa8
Qiwen Hua <hqw@huaqiwen.com>
2020-08-11 09:17:42 -0700

    add create new customer

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 /**
  * @desc This is the backend for communication between
  * the mobile ends and the Stripe API.
+ * 
  * @url https://stripe-server-test.herokuapp.com/.
  * @author Qiwen Hua
  */
 
+ 
 const express = require('express')
+const bodyParser = require('body-parser');
 const path = require('path')
-const PORT = process.env.PORT || 5000
 
 // This is currently a test secret that is for testing only
 const SECRET_KEY = 'sk_test_51HCrBpIazuHtyn2CnkySWfrMk70lU79XbuJLFg8Z1g8KC5HlvXwki8MOv5o2k4g7C0xdQmosXIZHJk1BS5LF7LFG00nTWPzboy'
-
-const bodyParser = require('body-parser');
 const stripe = require('stripe')(SECRET_KEY);
 
 // Prices for subscription plan charges
@@ -22,12 +22,32 @@ const subscriptionPlan = {
     "high" : 2300,
 }
 
+const PORT = process.env.PORT || 5000
+
+
 express()
     .use(express.static(path.join(__dirname, 'public')))
     .use(bodyParser.json())
     .use(bodyParser.urlencoded({ extended: true }))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
+
+    // Create a new customer by `name` and `email`
+    .post('/new_customer', (req, res) => {
+        console.log(req.body);
+        const name = req.body.name
+        const email = req.body.email
+
+        stripe.customers.create({
+            name: name,
+            email: email
+        }).then((customer) => {
+            res.status(200).send(customer.id)
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).end()
+        });
+    })
 
     // Fetch ephemeral keys by `customer_id` and `api_version`
     .post('/ephemeral_keys', (req, res) => {
@@ -41,7 +61,7 @@ express()
         ).then((key) => {
             res.status(200).send(key)
         }).catch((err) => {
-            console.log(err)
+            console.log(err) 
             res.status(500).end()
         });
     })
